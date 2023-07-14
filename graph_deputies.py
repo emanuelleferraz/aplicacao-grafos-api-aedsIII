07e370808001f29a91d72541ef9f87998d926fd4
Emanuelle Ferraz <111248050+emanuelleferraz@users.noreply.github.com>
2023-07-14 01:00:20 -0300

    Update graph_deputies.py

--- graph_deputies.py
+++ graph_deputies.py
@@ -31,26 +31,29 @@ class Graph:
       self.adj_list[node1][node2] = 1
       self.edge_count += 1
 
-  # Construção do Grafo de Deputados usando API
+  # Construção do Grafo de Deputados usando consumo de API
   def deputies_graph(self):
     favor = []
     against = []
-    a = amount_votes.AmountVotes()
+    a = AmountVotes()
     voting_ids = requests.get(
       "https://dadosabertos.camara.leg.br/api/v2/votacoes?dataInicio=2022-01-01&ordem=DESC&ordenarPor=dataHoraRegistro"
     )
 
     voting_ids = voting_ids.json()
 
-    for i in range(len(voting_ids["dados"])):
-      votings = voting_ids["dados"][i]
+    control_1 = 0
+    while control_1 < len(voting_ids["dados"]):
+      votings = voting_ids["dados"][control_1]
       voting = "https://dadosabertos.camara.leg.br/api/v2/votacoes/" + votings[
         "id"] + "/votos"
       votes = requests.get(voting)
       votes = votes.json()
-      for i in range(len(votes["dados"])):
+
+      control_2 = 0
+      while control_2 < len(votes["dados"]):
         if votes["dados"] != []:
-          analyze_vote = votes["dados"][i]
+          analyze_vote = votes["dados"][control_2]
           if analyze_vote["tipoVoto"] == "Sim":
             favor.append(analyze_vote["deputado_"]["nome"])
           elif analyze_vote["tipoVoto"] == "Não":
@@ -59,31 +62,47 @@ class Graph:
             a.adj_list[analyze_vote["deputado_"]["nome"]] = 1
           else:
             a.adj_list[analyze_vote["deputado_"]["nome"]] += 1
+        control_2 += 1
 
-      for deputy in favor:
-        for deputy2 in favor:
+      control_3 = 0
+      while control_3 < len(favor):
+        deputy = favor[control_3]
+        control_4 = 0
+        while control_4 < len(favor):
+          deputy2 = favor[control_4]
           if deputy == deputy2:
+            control_4 += 1
             continue
           self.add_edge(deputy, deputy2)
+          control_4 += 1
+        control_3 += 1
 
-      for deputy in against:
-        for deputy2 in against:
+      control_5 = 0
+      while control_5 < len(against):
+        deputy = against[control_5]
+        control_6 = 0
+        while control_6 < len(against):
+          deputy2 = against[control_6]
           if deputy == deputy2:
+            control_6 += 1
             continue
           self.add_edge(deputy, deputy2)
+          control_6 += 1
+        control_5 += 1
 
       favor = []
       against = []
+      control_1 += 1
 
     a.arquive()
 
   # Armazenando os dados no arquivo
   def write_arq(self):
-    dep_descobertos = []
+    dep_list = []
     with open("votacaoVotos-2022-graph.txt", 'w') as file:
       file.write(f"{self.node_count} {self.edge_count}\n")
       for dep in self.adj_list:
         for dep2 in self.adj_list[dep]:
-          if dep2 not in dep_descobertos:
+          if dep2 not in dep_list:
             file.write(f"{dep} {dep2} {self.adj_list[dep][dep2]}\n")
-        dep_descobertos.append(dep)
+        dep_list.append(dep)
